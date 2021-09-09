b580b164df0324e0e79f4981284f5416c7837591
brianpompey <brianthescholar@gmail.com>
2021-09-09 16:23:33 -0400

    Added an async await to auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,15 +3,19 @@ const User = require("../models/User");
 
 //REGISTER
 
-router.post("/register", (req,res) =>{
+router.post("/register", async (req,res) =>{
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
         password:req.body.password
     });
 
-    const user = newUser.save();
-    res.json(user)
+    try{
+        const user = await newUser.save();
+        res.sratus(201).json(user)
+    }catch(error){
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
