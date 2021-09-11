5991b56897ecd2a98e81b3cda5428aace5e6968c
brianpompey <brianthescholar@gmail.com>
2021-09-11 19:58:49 -0400

    had a big typo

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 
 dotenv.config();
@@ -20,9 +21,10 @@ async function main() {
 
 app.use(express.json());
 
-app.use("/app/auth", authRoute);
-app.use("/app/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend Server is running!")
