7200bf801bb75abce6a76a74962f66598da688c7
brianpompey <brianthescholar@gmail.com>
2021-09-11 13:53:05 -0400

    adding to user routes

---
+++ api/routes/users.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+//UPDATE
+
+//DELETE
+
+//GET
+
+//GET_ALL
+
+//GET_USER_STATS
